dc6a9fe4d9ff8429895a2fdd1e7830658a812401
Adam Betz <blarglephish@gmail.com>
2018-10-18 23:04:33 -0700

    Finishing Ch. 4

--- data-science-from-scratch/Project_PyCharm/visualizing_data.py
+++ data-science-from-scratch/Project_PyCharm/visualizing_data.py
@@ -83,17 +83,50 @@ def make_chart_misleading_y_axis(mislead=True):
         plt.title("Not So Huge Anymore")
     plt.show()
 
-# '''
-#  Example #5: Adding multiple series to a line chart
-# '''
-# def make_chart_several_line_charts():
-#     # TODO
-#
-# '''
-#  Example #6: Scatterplots for showing relationships between related data
-# '''
-# def make_chart_scatterplot_axes(equal_axes=False):
-#     # TODO
+'''
+ Example #5: Adding multiple series to a line chart
+'''
+def make_chart_several_line_charts():
+    variance = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+    bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+    total_error = [x + y for x, y in zip(variance, bias_squared)]
+    xs = [i for i, _ in enumerate(variance)]
+
+    # We can make multiple calls to plt.plot
+    # to show multiple series on the same chart
+    plt.plot(xs, variance,      'g-',   label='variance')       # green solid line
+    plt.plot(xs, bias_squared,  'r-.',  label='bias^2')         # red dot-dashed line
+    plt.plot(xs, total_error,   'b:',   label='total error')    # blue dotted line
+
+    # Because we've assigned labels to each series
+    # we can get a legend for free
+    # loc=9 means "top center"
+    plt.legend(loc=9)
+    plt.xlabel("model complexity")
+    plt.title("The Bias-Variance Tradeoff")
+    plt.show()
+
+'''
+ Example #6: Scatterplots for showing relationships between related data
+'''
+def make_chart_scatterplot_axes(equal_axes=False):
+    friends = [70, 65, 72, 63, 71, 64, 60, 64, 67]
+    minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
+    labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+    plt.scatter(friends, minutes)
+
+    # label each point
+    for label, friend_count, minute_count in zip(labels, friends, minutes):
+        plt.annotate(label,
+             xy=(friend_count, minute_count),   # put the label with its point
+             xytext=(5, -5),                    # but slightly offset
+             textcoords='offset points')
+
+    plt.title("Daily Minutes vs. Number of Friends")
+    plt.xlabel("# of friends")
+    plt.ylabel("daily minutes spent on the site")
+    plt.show()
 
 '''
  Main Routine
@@ -112,7 +145,7 @@ if __name__ == "__main__":
     #
     # make_chart_several_line_charts()
     #
-    # make_chart_scatterplot_axes(equal_axes=False)
+    make_chart_scatterplot_axes(equal_axes=False)
     #
     # make_chart_scatterplot_axes(equal_axes=True)
     #
